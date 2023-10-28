c4605587e690ad4059715a071cde5100fb60c046
Bufo <bufo24@users.noreply.github.com>
2023-10-28 13:59:04 +0200

    feat: init tsx support

---
+++ global.d.ts
@@ -0,0 +1,17 @@
+declare interface BosContext {
+  accountId?: string;
+  networkId: NetworkId;
+}
+
+declare const Widget: (params: {
+  src: string;
+  props: object;
+}) => React.ReactNode;
+
+declare interface BaseProps {
+  page: any;
+}
+
+declare var styled = {
+  div
+}
